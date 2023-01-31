b32ee3bed29fa7cd61b85fb2092046f6d3c63c85
Laura Moraes <118922125+LauraMoraees@users.noreply.github.com>
2023-01-31 01:13:22 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,112 @@
+
+//Interação
+const citySearchInput = document.getElementById('city-search-input')
+const citySearchButton = document.getElementById('city-search-button')
+
+//Exibição
+const currentDate = document.getElementById('current-date')
+const cityName = document.getElementById('city-name')
+const weatherIcon = document.getElementById('weather-icon')
+const weatherDescription = document.getElementById('weather-description')
+const currentTemperature = document.getElementById('current-temperature')
+const windSpeed = document.getElementById('wind-speed')
+const feelsLikeTemperature = document.getElementById('feels-like-temperature')
+const currentHumidity = document.getElementById('current-humidity')
+const sunsetTime = document.getElementById('sunset-time')
+const sunriseTime = document.getElementById('sunrise-time')
+let bgColor = "";
+
+const api_key = 'b0926c4638b1d89a1e02fac5b435ab10';
+
+citySearchButton.addEventListener('click', () => {
+
+  let cityName = citySearchInput.value
+
+  getCityWeather(cityName)
+})
+
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    let lat = position.coords.latitude
+    let lon = position.coords.longitude
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${api_key}`)
+  .then((response) => response.json())
+  .then((data) => displayWeather(data))
+},
+
+(err) => {
+  if(err.code === 1){
+    alert("Geolocalização negada pelo usuário. Busque manualmente por uma cidade ataravés da barra de pesquisa")
+  }
+}
+)
+
+function getCityWeather(cityName){
+  
+  weatherIcon.src = `./assets/loading-icon.svg`
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`)
+  .then((response) => response.json())
+  .then((data) => displayWeather(data))
+}
+
+function displayWeather(data){
+  let {
+    dt,
+    main : {temp, feels_like, humidity},
+    name,
+    sys : {sunrise, sunset},
+    weather : [{icon, description}],
+    wind : {speed}
+  } = data  
+ 
+  currentDate.textContent = formatDate(dt)
+  cityName.textContent = name
+  weatherIcon.src = `./assets/${icon}.svg`
+  weatherDescription.textContent = description
+  currentTemperature.textContent = `${Math.round(temp)}ºC`
+  windSpeed.textContent = `${Math.round(speed * 3.6)}km/h`
+  feelsLikeTemperature.textContent = `${Math.round(feels_like)}ºC`
+  currentHumidity.textContent = `${humidity}%`
+  sunsetTime.textContent = formatTime(sunset)
+  sunriseTime.textContent = formatTime(sunrise) 
+  
+  switch(description){
+    case "nublado":
+      bgColor = '#d5d6dd';
+    break;
+    case "algumas nuvens":
+      bgColor = '#85c5fb';
+    break;
+    case "nuvens dispersas":
+      bgColor = '#cbcfe9;';
+    break;
+    case "chuva leve":
+      bgColor = '#d5d6dd';
+    break;
+    case "chuva forte":
+      bgColor = '#c9bdbd';
+    break;
+    case "céu limpo":
+      bgColor = '#97e2ff';
+    break;
+    default:
+      bgColor = '#ffffff'; 
+  }
+
+   document.body.style.background = bgColor
+}
+
+
+function formatDate(epochTime){
+  let date = new Date(epochTime * 1000)
+  let formattedDate = date.toLocaleDateString('pt-BT', {month:"long", day:"numeric"})
+  return `Hoje, ${formattedDate}`
+}
+
+function formatTime(epochTime){
+  let date = new Date(epochTime * 1000)
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  return `${hours}:${minutes}`
+}
